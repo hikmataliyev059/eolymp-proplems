913d44261fa64fc7efdda5a8ca29bc658acef550
hikmet059 <javahikmet5909@gmail.com>
2024-02-05 15:04:06 +0400

    DivideTheApples2

---
+++ src/main/java/eo/classes/Basics/DivideTheApples2.java
@@ -0,0 +1,12 @@
+package eo.classes.Basics;
+
+import java.util.Scanner;
+
+public class DivideTheApples2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        System.out.println(b % a);
+    }
+}
