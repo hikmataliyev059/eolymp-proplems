b28593f3ba9c6a8f9708f2e094820e5f9b46c5c0
hikmet059 <javahikmet5909@gmail.com>
2024-02-05 15:23:04 +0400

    Divisibility

---
+++ src/main/java/eo/classes/ConditionalLoops/Divisibility.java
@@ -0,0 +1,16 @@
+package eo.classes.ConditionalLoops;
+
+import java.util.Scanner;
+
+public class Divisibility {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        if (a % b == 0) {
+            System.out.println("Divisible");
+        } else {
+            System.out.println(a / b + " " + a % b);
+        }
+    }
+}
