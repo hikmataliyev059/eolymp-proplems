bf0786deef539308ac37765aa2c8fffa103cd399
hikmet059 <javahikmet5909@gmail.com>
2024-02-08 01:29:17 +0400

    InsideTheInterval

---
+++ src/main/java/eo/classes/ConditionalLoops/InsideTheInterval.java
@@ -0,0 +1,17 @@
+package eo.classes.ConditionalLoops;
+
+import java.util.Scanner;
+
+public class InsideTheInterval {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        if (x >= a && x <= b) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
