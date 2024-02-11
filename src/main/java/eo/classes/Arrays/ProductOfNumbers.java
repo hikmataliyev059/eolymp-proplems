99950f9f3cf09a28c5585996e2714e743b9c5a92
hikmet059 <javahikmet5909@gmail.com>
2024-02-11 19:04:17 +0400

    ProductOfNumbers

---
+++ src/main/java/eo/classes/Arrays/ProductOfNumbers.java
@@ -0,0 +1,17 @@
+package eo.classes.Arrays;
+
+import java.util.Scanner;
+
+public class ProductOfNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int sum = 1;
+        for (int i = 0; i < 3; i++) {
+            int a = num % 10;
+            num /= 10;
+            sum *= a;
+        }
+        System.out.println(sum);
+    }
+}
