1f3cd78829466500c4d433c39177ceb1e6741a24
hikmet059 <javahikmet5909@gmail.com>
2024-02-16 20:57:14 +0400

    EvenOrOddGM

---
+++ src/main/java/eo/classes/GeneralMixed/EvenOrOddGM.java
@@ -0,0 +1,15 @@
+package eo.classes.GeneralMixed;
+
+import java.util.Scanner;
+
+public class EvenOrOddGM {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        if (n % 2 == 0) {
+            System.out.println("EVEN");
+        } else {
+            System.out.println("ODD");
+        }
+    }
+}
