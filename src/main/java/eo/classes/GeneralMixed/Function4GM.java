2c571fff97693639f80538517bfd7b18bffd9ee1
hikmet059 <javahikmet5909@gmail.com>
2024-02-18 15:43:15 +0400

    Function4GM

---
+++ src/main/java/eo/classes/GeneralMixed/Function4GM.java
@@ -0,0 +1,12 @@
+package eo.classes.GeneralMixed;
+
+import java.util.Scanner;
+
+public class Function4GM {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        long x = scan.nextLong();
+        long y = 1 + x + (x * x);
+        System.out.println(y);
+    }
+}
