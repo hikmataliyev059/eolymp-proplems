2cf38ed69cde9b171f75dce92c1d3d9770893cca
hikmet059 <javahikmet5909@gmail.com>
2024-02-21 21:10:03 +0400

    PerimeterOfRectangleGM

---
+++ src/main/java/eo/classes/GeneralMixed/PerimeterOfRectangleGM.java
@@ -0,0 +1,12 @@
+package eo.classes.GeneralMixed;
+
+import java.util.Scanner;
+
+public class PerimeterOfRectangleGM {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        System.out.println(2 * (a + b));
+    }
+}
