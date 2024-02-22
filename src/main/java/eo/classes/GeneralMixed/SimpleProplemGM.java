a6abe5de44769b79eaa878f2ed133cc3f421a926
hikmet059 <javahikmet5909@gmail.com>
2024-02-23 00:43:24 +0400

    SimpleFunctionGM

---
+++ src/main/java/eo/classes/GeneralMixed/SimpleProplemGM.java
@@ -0,0 +1,12 @@
+package eo.classes.GeneralMixed;
+
+import java.util.Scanner;
+
+public class SimpleProplemGM {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        System.out.println(a / 10);
+        System.out.println(a % 10);
+    }
+}
