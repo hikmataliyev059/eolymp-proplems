b4069add4ac35fd02ce00c096d150b72e95ab8a9
hikmet059 <javahikmet5909@gmail.com>
2024-03-03 01:24:23 +0400

    AlphabeticalOrderGM

---
+++ src/main/java/eo/classes/GeneralMixed/AlphabeticalOrderGM.java
@@ -0,0 +1,9 @@
+package eo.classes.GeneralMixed;
+
+public class AlphabeticalOrderGM {
+    public static void main(String[] args) {
+        for (char i = 'a'; i <= 'z'; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
