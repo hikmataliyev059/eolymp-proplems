b543c81447c8a3c9ecca91e160d30dfa4481cb6f
hikmet059 <javahikmet5909@gmail.com>
2024-03-04 00:40:36 +0400

    ConditionalOperator2

---
+++ src/main/java/eo/classes/ConditionalLoops/ConditionalOperator2.java
@@ -0,0 +1,17 @@
+package eo.classes.ConditionalLoops;
+
+import java.util.Scanner;
+
+public class ConditionalOperator2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        long x = scan.nextInt();
+        long y;
+        if (x >= 10) {
+            y = (x * x * x) + (5 * x);
+        } else {
+            y = (x * x) - (2 * x) + 4;
+        }
+        System.out.println(y);
+    }
+}
