7f5d56b42683bf980d08dc22ab6e88d7597a84a4
hikmet059 <javahikmet5909@gmail.com>
2024-03-05 01:38:58 +0400

    MultiplyNumbers

---
+++ src/main/java/eo/classes/ConditionalLoops/MultiplyNumbers.java
@@ -0,0 +1,15 @@
+package eo.classes.ConditionalLoops;
+
+import java.util.Scanner;
+
+public class MultiplyNumbers {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int first = num / 100;
+        int second = (num / 10) % 10;
+        int third = num % 10;
+        System.out.println(first * second + third);
+    }
+}
+
