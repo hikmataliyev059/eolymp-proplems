5f3a9aa5c469a7c7bd554e1796302e23caf2bdbf
hikmet059 <javahikmet5909@gmail.com>
2024-03-13 02:09:44 +0400

    HackerRank1

---
+++ src/main/java/eo/classes/Basics/HackerRank1.java
@@ -0,0 +1,16 @@
+package eo.classes.Basics;
+
+import java.util.Scanner;
+
+public class HackerRank1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        double b = scan.nextDouble();
+        scan.nextLine();
+        String c = scan.nextLine();
+        System.out.println("String: " + c);
+        System.out.println("Double: " + b);
+        System.out.println("Int: " + a);
+    }
+}
