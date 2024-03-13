bcebccf053a207137eae64e04f167ca4dfde346d
hikmet059 <javahikmet5909@gmail.com>
2024-03-14 03:15:29 +0400

    HackerRank3

---
+++ src/main/java/eo/classes/Basics/HackerRank3.java
@@ -0,0 +1,19 @@
+package eo.classes.Basics;
+
+import java.util.Scanner;
+
+public class HackerRank3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        if (num % 2 == 1) {
+            System.out.println("Weird");
+        } else if (num % 2 == 0 && num >= 2 && num <= 5) {
+            System.out.println("Not Weird");
+        } else if (num % 2 == 0 && num >= 6 && num <= 20) {
+            System.out.println("Weird");
+        } else if (num % 2 == 0 && num > 20) {
+            System.out.println("Not Weird");
+        }
+    }
+}
