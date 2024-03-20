7d739bd8d723a95fc958b5a0adaa754ceff2a004
hikmet059 <javahikmet5909@gmail.com>
2024-03-20 12:42:56 +0400

    SnakeMatrix

--- src/main/java/eo/classes/Arrays/SnakeMatrix.java
+++ src/main/java/eo/classes/Arrays/SnakeMatrix.java
@@ -3,19 +3,19 @@ package eo.classes.Arrays;
 import java.util.Scanner;
 
 public class SnakeMatrix {
-    public static void main(String[] args) {
+    public static void mainV1(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
         int[][] matrix = new int[n][n];
-        int num = 1;
+        int counter = 1;
         for (int i = 0; i < n; i++) {
             if (i % 2 == 0) {
                 for (int j = 0; j < n; j++) {
-                    matrix[i][j] = num++;
+                    matrix[i][j] = counter++;
                 }
             } else {
                 for (int j = n - 1; j >= 0; j--) {
-                    matrix[i][j] = num++;
+                    matrix[i][j] = counter++;
                 }
             }
         }
@@ -23,6 +23,24 @@ public class SnakeMatrix {
             for (int j = 0; j < n; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
+            System.out.println();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int counter = 1;
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                if (row % 2 == 0) {
+                    System.out.print(counter++ + "\t");
+                } else {
+                    System.out.print(--counter + "\t");
+                }
+            }
+            counter += n;
             System.out.println();
         }
     }
